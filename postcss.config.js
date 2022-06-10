6ed0bfb245b525310c73e9e558ea03fed22fe46f
Agung Setyawan <agung.setyawan@gdn-commerce.com>
2022-06-10 19:16:20 +0700

    add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,8 @@
-const tailwindcss = require('tailwindcss')
 module.exports = {
-  plugins: ['postcss-preset-env', tailwindcss]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano:
+      process.env.NODE_ENV === 'production' ? { preset: 'default' } : false
+  }
 }
